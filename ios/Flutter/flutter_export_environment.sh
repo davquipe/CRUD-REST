4c748f6c9f962bb31fb3779fb6efffa0b12ed9f2
David <davidcondori99@gmail.com>
2020-06-15 12:18:54 -0500

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/davidcondoriquispe/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/davidcondoriquispe/Downloads/flutter-form-validation-1.0.0"
+export "FLUTTER_TARGET=/Users/davidcondoriquispe/Downloads/flutter-form-validation-1.0.0/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/davidcondoriquispe/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
---
+++ lib/src/providers/productos_provider.dart
@@ -0,0 +1,67 @@
+
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:formvalidation/src/models/producto_model.dart';
+
+class ProductosProvider {
+
+  final String _url = 'https://flutter-varios-b1007.firebaseio.com';
+
+  Future<bool> createProducto( ProductoModel producto ) async {
+
+    final url = '$_url/productos.json';
+
+    final resp = await http.post( url, body: productoModelToJson(producto) );
+
+    final decodedData = json.decode(resp.body);
+
+    print( decodedData );
+
+    return true;
+
+  }
+
+  Future <List<ProductoModel>> cargarProductos() async {
+
+    final url ='$_url/productos.json';
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(resp.body);
+    final List<ProductoModel> productos = new List();
+
+    if (  decodedData == null ) return[];
+
+    decodedData.forEach((id, prod) {
+
+      final prodTemp = ProductoModel.fromJson(prod);
+      prodTemp.id = id;
+
+      productos.add( prodTemp );
+
+    });
+
+    print( productos );
+
+
+    return productos;
+
+  }
+
+
+  Future<int> borrarProducto( String id ) async {
+
+    final url = '$_url/productos/$id.json';
+    final resp = await http.delete(url);
+
+    print( resp.body );
+
+    return 1;
+
+  }
+
+
+}
+
+
